0090cd9a12f81fc3ce5961dab80df784e5d6dbd1
yangfourone <shiyi012129@gmail.com>
2020-02-25 12:48:05 +0800

    continuously predict

--- flask_.py
+++ flask_.py
@@ -4,9 +4,11 @@ import os.path
 import numpy as np
 import sys
 import subprocess
+from datetime import datetime
 from flask import Flask, jsonify, request
 from tensorflow.python.keras.models import load_model
 from tensorflow.python.keras.preprocessing import image
+from tensorflow.python.keras.backend import clear_session
 
 app = Flask(__name__)
 
@@ -15,33 +17,43 @@ cls_list = ['A-000', 'A-180', 'B-000', 'B-180', 'C-090', 'D-000', 'D-090', 'D-18
             'F-180', 'G-000', 'G-180', 'H-000', 'H-180', 'I-180', 'I-270', 'J-090']
 
 
-@app.route("/post-test", methods=["GET", "POST"])
+@app.route('/get-test', endpoint='get-test')
+def get_test():
+    path = 'dataset/test/user_20200224-144850.jpg'
+    result = recognition(path)
+    clear_session()
+    return jsonify(result)
+
+
+@app.route('/post-test', methods=['GET', 'POST'])
 def post_test():
-    if request.method == "POST":
+    if request.method == 'POST':
         parameter = dict(request.form)
-        value = parameter["pic"]
+        value = parameter['pic']
         return jsonify(value)
 
 
-@app.route("/upload-image", methods=["GET", "POST"])
+@app.route('/upload-image', methods=['GET', 'POST'])
 def upload_image():
-    if request.method == "POST":
-        path = "dataset/test/user.jpg"
+    if request.method == 'POST':
+        now = datetime.now()
+        current_time = now.strftime('%Y%m%d-%H%M%S')
+        path = 'dataset/test/user_' + str(current_time) + '.jpg'
         file = dict(request.form)
-        img_data = file["data"]
-        img_data = img_data.replace(" ", "+")
+        img_data = file['data']
+        img_data = img_data.replace(' ', '+')
         img_json = img_data.encode()
-
-        with open(path, "wb") as fh:
+        with open(path, 'wb') as fh:
             fh.write(base64.decodebytes(img_json))
 
         # recognition
-        recognition(path)
+        result = recognition(path)
 
         # delete file
+        clear_session()
         os.remove(path)
 
-        return jsonify('value')
+        return jsonify(result)
 
 
 def recognition(path):
@@ -50,12 +62,14 @@ def recognition(path):
     img = image.load_img(path, target_size=(224, 224))
     x = image.img_to_array(img)
     x = np.expand_dims(x, axis=0)
-    pred = net.predict(x)[0]
-    top_inds = pred.argsort()[::-1][:3]
+    predict = net.predict(x)[0]
+    top_index = predict.argsort()[::-1][:2]
     print(path)
-    for i in top_inds:
-        print('    {:.3f}  {}'.format(pred[i], cls_list[i]))
+    for index in top_index:
+        print('    {:.3f}  {}'.format(predict[index], cls_list[index]))
+
+    return cls_list[top_index[0]]
 
 
-ip = subprocess.check_output([sys.executable, "get_ip.py"]).decode()
+ip = subprocess.check_output([sys.executable, 'get_ip.py']).decode()
 app.run(host=ip, port=5000, debug=True)
